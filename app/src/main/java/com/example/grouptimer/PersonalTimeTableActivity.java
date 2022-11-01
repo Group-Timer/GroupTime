4d2740c67c5e4cd63c2d328d02da2318a676a770
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2022-11-01 13:01:52 +0900

    Activity

---
+++ app/src/main/java/com/example/grouptimer/PersonalTimeTableActivity.java
@@ -0,0 +1,14 @@
+package com.example.grouptimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalTimeTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_time_table);
+    }
+}
