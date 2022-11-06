846c29dbee82ae159d7f90c8a1e12c1976b3e8c8
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2022-11-06 18:51:56 +0900

    User

--- app/src/main/java/com/example/grouptimer/model/User.java
+++ app/src/main/java/com/example/grouptimer/model/User.java
@@ -3,9 +3,5 @@ package com.example.grouptimer.model;
 public class User {
     public String userName; // user 이름
     public int phoneNumber; // user 전화번호
-<<<<<<< HEAD
     public int groupNumber; // user 참여중인 group 수
-=======
-    public int groupNumber; // user 참여 group 수
->>>>>>> a5808a1c168c817965c869f85e971afae6e72c21
 }
