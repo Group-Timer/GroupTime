e80bc3332189e5ff1004caac9e7a68acac071b93
gomhyeok <gomhyeok@gmail.com>
2022-11-08 22:48:37 +0900

    search id.xml

--- app/src/main/java/com/example/grouptimer/SearchIDActivity.java
+++ app/src/main/java/com/example/grouptimer/SearchIDActivity.java
@@ -3,12 +3,22 @@ package com.example.grouptimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchIDActivity extends AppCompatActivity {
-
+    private Button searchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_id);
+
+        searchButton = (Button) findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
