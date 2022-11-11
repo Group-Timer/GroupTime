460a45cb19cef9305d2e80bca5f7f1196fa0b69a
gomhyeok <gomhyeok@gmail.com>
2022-11-11 20:07:33 +0900

    이름 변경

--- app/src/main/java/com/example/grouptimer/ChangeNameActivity.java
+++ app/src/main/java/com/example/grouptimer/ChangeNameActivity.java
@@ -1,14 +1,97 @@
 package com.example.grouptimer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.grouptimer.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ChangeNameActivity extends AppCompatActivity {
+    private String name;
+    private Button check;
+    private Button change;
+    private EditText input;
+    private boolean accept;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_name);
+        DatabaseReference root;
+
+        check = findViewById(R.id.checkButton);
+        change = findViewById(R.id.changeButton);
+        input = findViewById(R.id.nameEdit);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        accept = false;
+
+        name = input.getText().toString();
+
+        String uid = user.getUid();
+        root = FirebaseDatabase.getInstance().getReference("Users");
+
+
+        check.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                root.child("Users").child(name)
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String value = snapshot.getValue(String.class);
+
+                        if(value!=null){
+                            Toast.makeText(getApplicationContext(),"이미 존재하는 그룹명입니다.",Toast.LENGTH_SHORT).show();//토스메세지 출력
+                        }
+                        else{
+                            accept = true;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // 디비를 가져오던중 에러 발생 시
+                        //Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
+                    }
+                });
+            }
+        });
+
+        change.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(accept){
+                    root.child("User").child(uid).child("username").setValue(name);
+                    Toast.makeText(getApplicationContext(),"변경되었습니다.",Toast.LENGTH_SHORT);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"중복확인이 필요합니다.",Toast.LENGTH_SHORT);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/grouptimer/SearchIDActivity.java
+++ app/src/main/java/com/example/grouptimer/SearchIDActivity.java
@@ -1,24 +1,125 @@
 package com.example.grouptimer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.grouptimer.model.User;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SearchIDActivity extends AppCompatActivity {
     private Button searchButton;
+    private EditText editNumber;
+
+    private DatabaseReference rootRef;
+
+    private RecyclerView recyclerView;
+    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_id);
+        String id;
+        String Email;
+        int checkNumber = Integer.parseInt(editNumber.getText().toString());
 
         searchButton = (Button) findViewById(R.id.search_button);
+        editNumber = (EditText) findViewById(R.id.phoneEdit);
+        rootRef = FirebaseDatabase.getInstance().getReference("Users");
 
+        //clickListener override
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Query query =
+                FirebaseDatabase.getInstance().getReference("Users")
+                        .orderByChild("phoneNumber").equalTo(checkNumber);
+                query.addListenerForSingleValueEvent(valueEventListener);
             }
         });
+
+
     }
+    ValueEventListener valueEventListener = new ValueEventListener(){
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot){
+            if(dataSnapshot.exists()){
+
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
 }
+//
+//public class SearchIDActivity extends AppCompatActivity {
+//
+//    private RecyclerView recyclerView;
+//    private UserAdapter adapter;
+//    private List<User> artistList;
+//
+//    DatabaseReference dbArtists;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_search_id);
+//
+//        recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        artistList = new ArrayList<>();
+//        adapter = new UserAdapter(this, artistList);
+//        recyclerView.setAdapter(adapter);
+//
+//        //1. SELECT * FROM Artists
+//        dbArtists = FirebaseDatabase.getInstance().getReference("Artists");
+//
+//        //2. SELECT * FROM Artists WHERE id = "-LAJ7xKNj4UdBjaYr8Ju"
+//        Query query = FirebaseDatabase.getInstance().getReference("Artists")
+//                .orderByChild("id")
+//                .equalTo("-LAJ7xKNj4UdBjaYr8Ju");
+//        query.addListenerForSingleValueEvent(valueEventListener);
+//    }
+//
+//
+//    ValueEventListener valueEventListener = new ValueEventListener() {
+//        @Override
+//        public void onDataChange(DataSnapshot dataSnapshot) {
+//            artistList.clear();
+//            if (dataSnapshot.exists()) {
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    User artist = snapshot.getValue(User.class);
+//                    artistList.add(artist);
+//                }
+//                adapter.notifyDataSetChanged();
+//            }
+//        }
+//
+//        @Override
+//        public void onCancelled(DatabaseError databaseError) {
+//
+//        }
+//    };
+//}
