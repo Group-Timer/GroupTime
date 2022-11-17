197ef2ca35d4bfc4524e43195f93552c3ca7a0a3
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2022-11-17 11:35:06 +0900

    xml

--- app/src/main/java/com/example/grouptimer/MainActivity.java
+++ app/src/main/java/com/example/grouptimer/MainActivity.java
@@ -11,9 +11,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button register;
     private Button login;
-    private Button findID;
 
-    private Button TempButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +22,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         register = (Button) findViewById(R.id.register);
         login = (Button) findViewById(R.id.login);
 
-        findID = findViewById(R.id.findID);
 
 
         register.setOnClickListener(this);
         login.setOnClickListener(this);
 
-        findID.setOnClickListener(this);
     }
 
 
@@ -44,13 +40,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if(view == login)
         {
             startActivity(new Intent(this, LoginActivity.class));
-        }
-        else if(view == findID) {
-            startActivity(new Intent(this, SearchIDActivity.class));
-        }
-        else if(view == TempButton)
-        {
-            int testA;
         }
     }
 }
